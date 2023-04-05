7582bd56f78dd0911168178053da0e9b7d049fdd
home409ca <114701129+home409ca@users.noreply.github.com>
2023-04-05 17:40:56 +0200

    Create __init__.py

---
+++ """Wtyczka zapisująca wybrane encje w panelu historii."""  DOMAIN = "history_save_entities" VERSION = "0.1.0" ISSUE_URL = "https:/github.com/username/history-save-entities/__init__.py
@@ -0,0 +1,9 @@
+"""Wtyczka zapisująca wybrane encje w panelu historii."""
+
+DOMAIN = "history_save_entities"
+VERSION = "0.1.0"
+ISSUE_URL = "https://github.com/username/history-save-entities/issues"
+
+def setup(hass, config):
+    """Konfiguracja wtyczki."""
+    return True
