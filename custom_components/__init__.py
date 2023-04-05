8cdde8d6ce0be48603b1b380ae2b73bcadfa42b1
home409ca <114701129+home409ca@users.noreply.github.com>
2023-04-05 18:21:15 +0200

    Update and rename __init__.py to custom_components/__init__.py

--- custom_components/__init__.py
+++ custom_components/__init__.py
@@ -2,7 +2,7 @@
 
 DOMAIN = "history_save_entities"
 VERSION = "0.1.0"
-ISSUE_URL = "https://github.com/username/history-save-entities/issues"
+ISSUE_URL = "https://github.com/home409ca/history-save-entities/issues"
 VERSION = "0.1.1"
 ISSUE_URL = "https://github.com/home409ca/history-save-entities/issues"
 
