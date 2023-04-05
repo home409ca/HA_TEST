65e2c30ef57fbf45e41315da7bd2dde18a4112f6
home409ca <114701129+home409ca@users.noreply.github.com>
2023-04-05 17:42:35 +0200

    Update __init__.py

--- """Wtyczka zapisująca wybrane encje w panelu historii."""  DOMAIN = "history_save_entities" VERSION = "0.1.0" ISSUE_URL = "https:/github.com/username/history-save-entities/__init__.py
+++ """Wtyczka zapisująca wybrane encje w panelu historii."""  DOMAIN = "history_save_entities" VERSION = "0.1.0" ISSUE_URL = "https:/github.com/username/history-save-entities/__init__.py
@@ -1,9 +1,16 @@
 """Wtyczka zapisująca wybrane encje w panelu historii."""
 
 DOMAIN = "history_save_entities"
-VERSION = "0.1.0"
-ISSUE_URL = "https://github.com/username/history-save-entities/issues"
+VERSION = "0.1.1"
+ISSUE_URL = "https://github.com/home409ca/history-save-entities/issues"
+
+DEFAULT_OPTIONS = {
+    "entities": []
+}
 
 def setup(hass, config):
     """Konfiguracja wtyczki."""
+    options = dict(DEFAULT_OPTIONS)
+    options.update(config.get(DOMAIN, {}))
+    hass.data[DOMAIN] = options
     return True
